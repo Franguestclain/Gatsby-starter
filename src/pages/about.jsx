ff176d174023edd3c847b2d5863211bd289fed32
Carlos Ortega <the_king_00carlos@hotmail.com>
2021-03-31 20:45:01 -0700

    Some really cool starting shit over here

---
+++ src/pages/about.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from '../components/header';
+
+export default function About() {
+  return <div style={{ color: `teal`}}>
+    <Header>About Gatsby</Header>
+    <p>Such wow. Very React.</p>
+  </div>
+}
